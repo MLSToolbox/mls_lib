0754b3b6a3c9f9790bd99b7076d50d9638d3c4e8
MikoMikarro <lopezcuestam@gmail.com>
2024-07-25 19:24:57 +0200

    fix syntax error

--- mls_lib/feature_engineering/data_transformer.py
+++ mls_lib/feature_engineering/data_transformer.py
@@ -1,7 +1,7 @@
 from . feature_engineering_step import FeatureEngineeringStep
 
 class DataTransformer(FeatureEngineeringStep):
-    def __init__(self, origins**) -> None:
+    def __init__(self, **origins) -> None:
         super().__init__()
 
         for key, value in origins:
