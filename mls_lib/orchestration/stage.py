1034785433b77355d1be53769c333eac0d8be210
MikoMikarro <lopezcuestam@gmail.com>
2024-10-15 09:05:21 +0200

    change step to tasks

--- mls_lib/orchestration/stage.py
+++ mls_lib/orchestration/stage.py
@@ -1,6 +1,6 @@
 """class Stage: Represents a stage in the pipeline. """
 import uuid
-from mls_lib.orchestration.step import Step
+from mls_lib.orchestration.task import Step
 
 class Stage(Step):
     """ Represents a stage in the pipeline. """
@@ -8,48 +8,48 @@ class Stage(Step):
     def __init__(self):
         """ Initializes a new instance of the class. """
         super().__init__()
-        self.steps = {}
+        self.tasks = {}
 
-    def add_step(self, step, **inputs):
-        """ Adds a step to the stage. """
-        step_key = str(uuid.uuid4())
-        self.steps[step_key] = (step, inputs)
-    def add_output(self, port, step_port):
+    def add_task(self, task, **inputs):
+        """ Adds a task to the stage. """
+        task_key = str(uuid.uuid4())
+        self.tasks[task_key] = (task, inputs)
+    def add_output(self, port, task_port):
         """ Adds an output to the stage. """
-        self.outputs[port] = step_port
+        self.outputs[port] = task_port
 
     def get_output(self, port):
         """ Get the output of the stage. """
-        output_step, output_port = self.outputs[port]
-        return output_step.get_output(output_port)
+        output_task, output_port = self.outputs[port]
+        return output_task.get_output(output_port)
 
     def execute(self):
-        """ Executes all the steps in the stage. """
-        step_keys = list(self.steps.keys())
+        """ Executes all the tasks in the stage. """
+        task_keys = list(self.tasks.keys())
         finish_count = 0
-        while finish_count < len(self.steps):
-            for step_key in step_keys:
-                if not self.__is_step_ready(step_key):
+        while finish_count < len(self.tasks):
+            for task_key in task_keys:
+                if not self.__is_task_ready(task_key):
                     continue
-                step, inputs = self.steps[step_key]
+                task, inputs = self.tasks[task_key]
                 data = {}
-                for port, step_port in inputs.items():
-                    input_step, input_port = step_port
-                    data[port] = input_step.get_output(input_port)
+                for port, task_port in inputs.items():
+                    input_task, input_port = task_port
+                    data[port] = input_task.get_output(input_port)
                 if len(data) > 0:
-                    step.set_data(**data)
-                step.execute()
-                step.finish_execution()
+                    task.set_data(**data)
+                task.execute()
+                task.finish_execution()
                 finish_count += 1
         self.finish_execution()
-    def __is_step_ready(self, step_key):
-        """ Checks if the step is ready. """
-        step, inputs = self.steps[step_key]
-        if step.is_finished():
+    def __is_task_ready(self, task_key):
+        """ Checks if the task is ready. """
+        task, inputs = self.tasks[task_key]
+        if task.is_finished():
             return False
-        for _, input_step_port in inputs.items():
-            input_step, _ = input_step_port
-            if not input_step.is_finished():
+        for _, input_task_port in inputs.items():
+            input_task, _ = input_task_port
+            if not input_task.is_finished():
                 return False
         return True
     
