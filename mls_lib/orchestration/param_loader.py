07e70913d2cc7cee315c49c95cad1285e3b66309
MikoMikarro <lopezcuestam@gmail.com>
2024-10-27 14:08:50 +0100

    updated param_loader

--- mls_lib/orchestration/param_loader.py
+++ mls_lib/orchestration/param_loader.py
@@ -1,15 +1,21 @@
 import os
 import yaml
 
+PARAM_FILE = os.environ.get("PARAM_FILE", "./params.yaml")
+
 class ParamLoader:
-    def __init__(self):
-        self.param_file = os.environ.get("PARAM_FILE", "params.yaml")
     @staticmethod
-    def load(self, param_name):
-        with open(self.param_file, 'r') as stream:
+    def load(param_name):
+        with open(PARAM_FILE, 'r') as stream:
             try:
                 params = yaml.safe_load(stream)
-                return params[param_name]
+                result = ParamLoader.recursive_get(params, param_name.split('.'))
+                return result
             except yaml.YAMLError as exc:
                 print(exc)
                 return None
+    @staticmethod
+    def recursive_get(params, param_name_list):
+        for param_name in param_name_list:
+            params = params[param_name]
+        return params
