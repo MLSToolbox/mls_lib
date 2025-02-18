2dd9f6b2f0f9f2047993dbf93d5e4a24b64b0441
MikoMikarro <lopezcuestam@gmail.com>
2025-02-18 20:14:12 +0100

    added separator to csv_loader

--- mls_lib/data_collection/csv_loader.py
+++ mls_lib/data_collection/csv_loader.py
@@ -7,12 +7,13 @@ from mls_lib.orchestration.task import Task
 
 class CSVLoader(Task):
     """ CSV Loader """
-    def __init__(self, path : str) -> None:
+    def __init__(self, path : str, separator : str) -> None:
         super().__init__()
         self.path = path
+        self.separator = separator
 
     def execute(self) -> None:
         df = DataFrame()
-        data = pd.read_csv(self.path)
+        data = pd.read_csv(self.path, sep = self.separator)
         df.set_data(data)
         self._set_output("out", df)
